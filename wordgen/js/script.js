47baaa10f36615f0ee0b04b9e7f36af7816822e9
-- <17591127+0x4kgi@users.noreply.github.com>
2021-07-19 12:57:11 +0800

    add mode const function array

--- wordgen/js/script.js
+++ wordgen/js/script.js
@@ -4,22 +4,41 @@ const SORT = {
   DESCENDING: 2,
 };
 
+const MODE = {
+  syllabic: function(syllables) {
+    return syllabicWordGen(syllables);
+  },
+  natural: function(length) {
+    return naturalWordGen(length);
+  }
+};
+
 const wordListDiv = document.getElementById('word-list');
 const regenButton = document.getElementById('reroll-words');
 const syllableInput = document.getElementById('syllables');
 const countInput = document.getElementById('count');
 const sortInput = document.getElementById('sort-mode');
 
+const consonants = 'bcdfghjklmnpqrstvwxz';
+const vowels = 'aeiouy'; // making y as a vowel coz it sounds like one
+
 const randomInt = (maxNumber) => {
   return Math.floor(Math.random() * maxNumber);
 };
 
 const syllabicWordGen = (syllables) => {
-  const consonants = 'bcdfghjklmnpqrstvwxz';
-  const vowels = 'aeiouy'; // making y as a vowel coz it sounds like one
-
   let generatedWord = '';
   for (let s = 0; s < syllables; s++) {
+    generatedWord += consonants[randomInt(consonants.length)];
+    generatedWord += vowels[randomInt(vowels.length)];
+  }
+
+  return generatedWord;
+};
+
+const naturalWordGen = (length) => {
+  let generatedWord = '';
+  for (let s = 0; s < length; s++) {
     let isSingle = randomInt(2);
 
     if (isSingle === 1) {
@@ -27,22 +46,18 @@ const syllabicWordGen = (syllables) => {
     } else {
       generatedWord += consonants[randomInt(consonants.length)];
       generatedWord += vowels[randomInt(vowels.length)];
-    }    
+    }
   }
-
   return generatedWord;
 };
 
-const naturalWordGen = (length) => {
-};
-
-const showWords = (words, syllableCount, sort = SORT.NONE) => {
+const showWords = (words, length, sort = SORT.NONE) => {
   wordListDiv.innerHTML = '';
 
   let wordsArray = [];
 
   for (let w = 0; w < words; w++) {
-    wordsArray.push(syllabicWordGen(syllableCount));
+    wordsArray.push(naturalWordGen(length));
   }
 
   if (sort == SORT.ASCENDING) { wordsArray.sort(); }
