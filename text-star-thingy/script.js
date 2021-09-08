42ab677178335b6f4cd324b3b303d8e2155e10b9
-- <17591127+0x4kgi@users.noreply.github.com>
2021-09-08 11:09:05 +0800

    text star :D

--- text-star-thingy/script.js
+++ text-star-thingy/script.js
@@ -3,19 +3,35 @@
 const input = document.getElementById('input');
 const output = document.getElementById('output');
 const separator = document.getElementById('sep');
+const alignment = document.getElementById('alignment');
 
-const createStar = (keyup) => {
-  const value = keyup.target.value;
-  const sep = separator.value;
+const createStar = () => {
+  const value = input.value;
+  const sep = separator.value || ' ';
+  const align = alignment.value;
 
   let outputString = '';
 
+  const maxLength = (value.length ** 2) * Math.max(sep.length, 1);
+
   for (let i = -(value.length); i <= value.length; i++) {
-    outputString += value.split('').join(sep.repeat(i < 0 ? -i : i)) + '<br>';
+    const line = value.split('').join(sep.repeat(i < 0 ? -i : i));
+
+    if (align === 'center') {
+      const space = (maxLength - line.length) / 2;
+      outputString += `${' '.repeat(space)}${line}<br>`;
+    } else if (align === 'right') {
+      const space = maxLength - line.length;
+      outputString += `${' '.repeat(space)}${line}<br>`;
+    } else {
+      outputString += line + '<br>';
+    }
   }
 
   output.innerHTML = outputString;
 };
 
 
-input.addEventListener('keyup', createStar);
+input.addEventListener('change', createStar);
+separator.addEventListener('change', createStar);
+alignment.addEventListener('change', createStar);
