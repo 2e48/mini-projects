2feb00f5af7c6b914d5af21634017ea9405fe24f
-- <17591127+0x4kgi@users.noreply.github.com>
2021-10-16 13:33:29 +0800

    randomness!

--- canvas/squares/index.js
+++ canvas/squares/index.js
@@ -4,18 +4,28 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 let squares = [];
-let colors = ['blue', 'red', 'green', 'yellow', 'brown', 'black'];
+let colors = ['blue', 'red', 'green',];
 
 function rand(max) {
   return Math.floor(Math.random() * max);
 }
 
+function randomColor() {
+  let r = rand(255);
+  let g = rand(255);
+  let b = rand(255);
+
+  return `rgb(${r},${g},${b})`;
+}
+
 function init() {
-  for (let i = 0; i < 10000; i++) {
+  for (let i = 0; i < 35; i++) {
     let s = Math.max(10, rand(25));
 
     let sq = new MainSquare(ctx, rand(canvas.width), rand(canvas.height), s, s)
-      .setColor(colors[i % colors.length]);
+      .setColor(randomColor())
+      .setSpeedX(Math.max(1, rand(10)))
+      .setSpeedY(Math.max(1, rand(10)));
 
     if (rand(100) > 49) {
       sq.flipX();
@@ -49,37 +59,38 @@ function squareUpdate(sq, sp = 1) {
 
   let xBound = canvas.width - sq.w;
   let yBound = canvas.height - sq.h;
-  let speed = sp;
+  let speedX = sq.speedX;
+  let speedY = sq.speedY;
 
   if (sq.toLeft) {
     if (x > xBound) {
       sq.flipX();
-      x -= speed;
+      x -= speedX;
     } else {
-      x += speed;
+      x += speedX;
     }
   } else {
     if (x < 0) {
       sq.flipX();
-      x += speed;
+      x += speedX;
     } else {
-      x -= speed;
+      x -= speedX;
     }
   }
 
   if (sq.downwards) {
     if (y > yBound) {
       sq.flipY();
-      y -= speed;
+      y -= speedY;
     } else {
-      y += speed;
+      y += speedY;
     }
   } else {
     if (y < 0) {
       sq.flipY();
-      y += speed;
+      y += speedY;
     } else {
-      y -= speed;
+      y -= speedY;
     }
   }
 
@@ -87,8 +98,8 @@ function squareUpdate(sq, sp = 1) {
 }
 
 function draw() {
-  ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   squares.forEach(i => i.draw());
 }
