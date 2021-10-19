ac8b4c2e66b142c67f7a771c2932ed319b0ccdaa
-- <17591127+0x4kgi@users.noreply.github.com>
2021-10-19 13:38:17 +0800

    ray logic!

--- canvas/osu-keyoverlay/index.js
+++ canvas/osu-keyoverlay/index.js
@@ -6,22 +6,39 @@ const context = canvas.getContext('2d');
 const KEY = {
   Z: 90,
   X: 88,
+  NUM1: 97,
+  NUM3: 99,
 };
 
 let input = {
   k1: false,
   k2: false,
+  m1: false,
+  m2: false,
 }
 
 let oldTimeStamp = 0;
 let secondsPassed = 0;
 
-let k1 = new KeySquare(context, 10, 10, 50, 50);
-let k2 = new KeySquare(context, 10, 70, 50, 50);
+let keys = [
+  new KeySquare(context, 10, 10, 50, 50, 'k1').setActiveColor('white').setColor('gray'),
+  new KeySquare(context, 10, 70, 50, 50, 'k2').setActiveColor('white').setColor('gray'),
+  new KeySquare(context, 10, 130, 50, 50, 'm1').setActiveColor('white').setColor('gray'),
+  new KeySquare(context, 10, 190, 50, 50, 'm2').setActiveColor('white').setColor('gray'),
+];
 
 let rays = {
   k1: [],
   k2: [],
+  m1: [],
+  m2: [],
+};
+
+let hasSpawnedRay = {
+  k1: false,
+  k2: false,
+  m1: false,
+  m2: false,
 };
 
 function initialize() {
@@ -32,30 +49,73 @@ function frameLoop(timeStamp) {
   secondsPassed = (timeStamp - oldTimeStamp) / 1000;
   oldTimeStamp = timeStamp;
 
-  update();
+  update(secondsPassed);
   clearCanvas();
   draw();
 
   window.requestAnimationFrame(frameLoop);
 }
 
-function update() {
-  if (input.k1) {
-    k1.activate();
-  } else {
-    k1.deactivate();
-  }
+function update(secondsPassed) {
+  keys.forEach(key => {
+    if (input[key.name]) {
+      key.activate();
 
-  if (input.k2) {
-    k2.activate();
-  } else {
-    k2.deactivate();
-  }
+      if (!hasSpawnedRay[key.name]) {
+        spawnRay(key).expand();
+        hasSpawnedRay[key.name] = true;
+      }
+    } else {
+      key.deactivate();
+
+      if (hasSpawnedRay[key.name]) {
+        const latestRay = rays[key.name][rays[key.name].length - 1];
+        latestRay.move();
+        hasSpawnedRay[key.name] = false;
+      }
+    }
+
+    rays[key.name].forEach((ray, index) => {
+      if (ray.x > canvas.width + key.x + key.w) {
+        rays[key.name].splice(index, 1);
+      } else {
+        ray.update(secondsPassed);
+      }
+    });
+  });
+}
+
+function spawnRay(baseKey) {
+  const key = baseKey.name;
+  const x = baseKey.x + baseKey.w;
+  const y = baseKey.y;
+  const w = 0;
+  const h = baseKey.h;
+  const ctx = baseKey.context;
+
+  const ray = new KeyRay(context, x, y, w, h)
+    .setColor(baseKey.activeColor)
+    .setScrollSpeed(500);
+
+  rays[key].push(ray);
+
+  return ray;
 }
 
 function draw() {
-  k1.draw();
-  k2.draw();
+  keys.forEach(key => {
+    key.draw();
+
+    rays[key.name].forEach(ray => ray.draw());
+  });
+
+  const grd = context.createLinearGradient(0, 0, canvas.width * 0.50, 0);
+  grd.addColorStop(0, 'rgba(0,0,0,0)');
+  //grd.addColorStop(.5, 'rgba(0,0,0,0.75)');
+  grd.addColorStop(1, 'rgba(0,0,0,1)');
+
+  context.fillStyle = grd;
+  context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function clearCanvas() {
@@ -68,9 +128,9 @@ document.body.addEventListener('keydown', evt => {
   switch (code) {
     case KEY.Z: input.k1 = true; break;
     case KEY.X: input.k2 = true; break;
+    case KEY.NUM1: input.m1 = true; break;
+    case KEY.NUM3: input.m2 = true; break;
   }
-
-  //console.log(input);
 });
 
 document.body.addEventListener('keyup', evt => {
@@ -79,9 +139,9 @@ document.body.addEventListener('keyup', evt => {
   switch (code) {
     case KEY.Z: input.k1 = false; break;
     case KEY.X: input.k2 = false; break;
+    case KEY.NUM1: input.m1 = false; break;
+    case KEY.NUM3: input.m2 = false; break;
   }
-
-  //console.log(input);
 });
 
 initialize();
