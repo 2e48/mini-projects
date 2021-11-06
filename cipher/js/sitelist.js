52161d85abb5cf110fb7bf5a8f22fbd4b7f08bb7
-- <17591127+0x4kgi@users.noreply.github.com>
2021-11-06 17:14:44 +0800

    yeeted sitelist.js

--- cipher/js/sitelist.js
+++
@@ -1,9 +0,0 @@
-const sites = {
-  's.d.u': 'safebooru.donmai.us',
-  'd.d.u': 'danbooru.donmai.us',
-  'w.p.n': 'www.pixiv.net',
-  'p.n': 'pixiv.net',
-  'm.t.c': 'mobile.twitter.com',
-  't.c': 'twitter.com',
-  'w.t.c': 'www.twitter.com',
-};
