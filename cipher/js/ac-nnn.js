3f3dcd9018879d6d1a88b544e08ac3c3c7865042
-- <17591127+0x4kgi@users.noreply.github.com>
2021-11-06 17:15:27 +0800

    decoder functionality, also added copying thingy

--- cipher/js/ac-nnn.js
+++ cipher/js/ac-nnn.js
@@ -1,23 +1,42 @@
 'use strict';
 
 const linkInput = document.getElementById('raw-link');
-const keyInput = document.getElementById('key');
-const randomKey = document.getElementById('key-random');
 const generateButton = document.getElementById('gen-key');
 const output = document.getElementById('output-lol');
-const referLink = document.getElementById('a-href-decoder');
+const copyButton = document.getElementById('copy');
 
 const cipher = new EncryptSauce();
 
-const generateKey = () => {
-  keyInput.value = cipher.randomKey();
-}
+const copyToClipboard = (what) => {
+  navigator.clipboard.writeText(what);
+};
 
-randomKey.addEventListener('click', () => generateKey());
 generateButton.addEventListener('click', () => {
-  if (keyInput.value == '') {
-    generateKey();
-  }
+  output.value = cipher.encode(linkInput.value, cipher.randomKey());
+});
 
-  output.value = cipher.encode(linkInput.value, keyInput.value);
+copyButton.addEventListener('click', () => {
+  copyToClipboard(output.value);
+  copyButton.innerHTML = 'Copied!';
+});
+
+// decoding logic
+
+const decodeIntro = document.getElementById('decode-intro');
+const decodeKey = document.getElementById('decode-button');
+const decodeOutro = document.getElementById('decode-outro');
+const decodeOpen = document.getElementById('decoded-open');
+const decodeCopy = document.getElementById('decoded-copy');
+
+decodeKey.addEventListener('click', () => {
+  decodeOutro.value = cipher.decode(decodeIntro.value);
+});
+
+decodeOpen.addEventListener('click', () => {
+  window.open(decodeOutro.value);
+});
+
+decodeCopy.addEventListener('click', () => {
+  copyToClipboard(decodeOutro.value);
+  decodeCopy.innerHTML = 'Copied!';
 });
