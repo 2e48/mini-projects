931286afa7a64208b12536799e01ece1c5f1b77e
-- <17591127+0x4kgi@users.noreply.github.com>
2022-01-06 16:39:56 +0800

    prevent fave dupes, +fave class...?

---
+++ wordgen/js/fave-handler.js
@@ -0,0 +1,38 @@
+class FaveWords {
+  /**
+   * @type string[]
+   */
+  #wordList;
+
+  constructor() {
+    this.clearAll();
+  }
+
+  clearAll() {
+    this.#wordList = [];
+  }
+
+  add(word) {
+    this.#wordList.push(word);
+  }
+
+  exists(word) {
+    return this.#wordList.includes(word);
+  }
+
+  remove(word) {
+    if (this.exists(word)) {
+      const index = this.#wordList.indexOf(word);
+
+      this.#wordList = this.#wordList.splice(index, 1);
+
+      return true;
+    }
+
+    return false;
+  }
+
+  listAll() {
+    return this.#wordList;
+  }
+}
